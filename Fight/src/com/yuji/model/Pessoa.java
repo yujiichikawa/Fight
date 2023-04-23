8cfb09f7ec6cfd744f0ce3cb648679cd43df8e7f
yujiichikawa <thiagogelinski20@gmail.com>
2023-04-23 11:11:44 -0300

    Criação da Classe Pessoa

---
+++ Fight/src/com/yuji/model/Pessoa.java
@@ -0,0 +1,48 @@
+package com.yuji.model;
+
+public abstract class Pessoa {
+	private String nome;
+	private int idade;
+	private float peso;
+	private String historia;
+	
+	public Pessoa(String nome, int idade, float peso, String historia) {
+		this.nome = nome;
+		this.idade = idade;
+		this.peso = peso;
+		this.historia = historia;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public String getHistoria() {
+		return historia;
+	}
+
+	public void setHistoria(String historia) {
+		this.historia = historia;
+	}
+	
+}
